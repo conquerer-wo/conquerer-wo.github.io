64fb0afa3901d2efc1ed049a57147e6a5f4a6c49
conquerer-wo <remiss_suit.4d@icloud.com>
2025-05-24 08:49:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,22 +3,32 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add('show');
+
+      // Add 'one' after 4400ms
+      setTimeout(() => {
+        entry.target.classList.add('one');
+
+        // Once 'one' is added, remove 'none'
+        if (entry.target.classList.contains('one')) {
+          entry.target.classList.remove('none', 'animate');
+          entry.target.classList.add('navg')
+        }
+      }, 4400);
     }
-    else { entry.target.classList.remove('show');}
   });
 }, { threshold: 0 });
 
 const elements = document.querySelectorAll('.animate');
 elements.forEach(element => observer.observe(element));
 
-// Second observer (defined before use)
+// Second observer
 const observer1 = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add('na');
     }
   });
-}, { threshold: 0.15});
+}, { threshold: 0.15 });
 
 const nav = document.querySelectorAll('.nav');
 nav.forEach(element => observer1.observe(element));
